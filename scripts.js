0b9b42e37b4d56f3d095235babca4de7da73150d
Sambu Nabulu <rachealsambu5@gmail.com>
2019-02-18 10:56:20 +0300

    add functions

--- scripts.js
+++ scripts.js
@@ -2,13 +2,7 @@ var triangle= function getTriangle(){
 var a = parseFloat(prompt("side a").value);
 var b = parseFloat(prompt("side b").value);
 var c = parseFloat(prompt("side c").value);
-var triangleType = function(side a, side b, side c){
-if (isTriangle(side a, side b, side c)){
-return triangleType(side a, side b, side c)}
-else{
-  return alert("These sides do not make a triangle");
-}
-};
+
  if ((side a === side b) && (side a === side c ) && (side b === side c)){
 return alert("Equilateral");
 }
@@ -18,6 +12,4 @@ return alert("Isosceles");
 else if(((side a + side b)>= side c) && ((side b +side c)>=a) && ((side a +side c)>=side b) {
   return alert("Scalene");
 }
-}
 };
-getTriangle();
